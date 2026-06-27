67854342be75f1618ea92073b7e0508c474a9c33
IvanDarma-S <ivandarma3000@gmail.com>
2026-06-28 01:08:44 +0700

    modifikasi kode route.go

--- routes/routes.go
+++ routes/routes.go
@@ -12,8 +12,30 @@ import (
 	"gorm.io/gorm"
 )
 
+// Fungsi Middleware CORS ditaruh di sini agar praktis
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Izinkan Flutter Web Anda mengakses API
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		// JIKA METHOD ADALAH OPTIONS (PREFLIGHT REQUEST FROM CHROME), LANGSUNG RESPON 204
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func Setup(db *gorm.DB, rdb *redis.Client, firebaseApp *firebase.App, cfg *config.Config) *gin.Engine {
 	r := gin.New()
+	
+	// --- PASANG MIDDLEWARE CORS DI SINI ---
+	r.Use(CORSMiddleware())
+	
 	r.Use(gin.Recovery())
 	r.Use(middleware.Logger())
 
