72335b84fbafc808ca601d6d8ac39d866363b3c7
IvanDarma-S <ivandarma3000@gmail.com>
2026-06-28 10:27:42 +0700

    feat:
    fix callback OTP error

--- services/email.go
+++ services/email.go
@@ -17,6 +17,22 @@ func NewEmailService(cfg *config.Config) *EmailService {
 	return &EmailService{cfg: cfg}
 }
 
+// func (s *EmailService) SendHTML(to, subject, htmlBody string) error {
+// 	if s.cfg.SMTPUser == "" {
+// 		return fmt.Errorf("SMTP not configured")
+// 	}
+
+// 	m := gomail.NewMessage()
+// 	m.SetAddressHeader("From", s.cfg.SMTPFrom, s.cfg.SMTPFromName)
+// 	m.SetHeader("To", to)
+// 	m.SetHeader("Subject", subject)
+// 	m.SetBody("text/html", htmlBody)
+
+// 	d := gomail.NewDialer(s.cfg.SMTPHost, s.cfg.SMTPPort, s.cfg.SMTPUser, s.cfg.SMTPPassword)
+// 	d.TLSConfig = &tls.Config{ServerName: s.cfg.SMTPHost}
+
+// 	return d.DialAndSend(m)
+// }
 func (s *EmailService) SendHTML(to, subject, htmlBody string) error {
 	if s.cfg.SMTPUser == "" {
 		return fmt.Errorf("SMTP not configured")
@@ -28,8 +44,14 @@ func (s *EmailService) SendHTML(to, subject, htmlBody string) error {
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", htmlBody)
 
+
 	d := gomail.NewDialer(s.cfg.SMTPHost, s.cfg.SMTPPort, s.cfg.SMTPUser, s.cfg.SMTPPassword)
-	d.TLSConfig = &tls.Config{ServerName: s.cfg.SMTPHost}
+    
+    d.SSL = false 
+    
+    d.TLSConfig = &tls.Config{
+        ServerName: s.cfg.SMTPHost,
+    }
 
 	return d.DialAndSend(m)
 }
